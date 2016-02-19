02faf3bcb36cf640a25b9bd9989240a8db2ed320
Andrew Heald <andrew@heald.co.uk>
2016-02-19 17:02:18 +0000

    Also write out a "latest" backup.

--- opt/backup.sh
+++ opt/backup.sh
@@ -2,7 +2,10 @@
 echo Reconstructing cache ...
 tarsnap --fsck
 echo Tarsnap Backup ...
+echo Paths:    $@
 filename=$FILENAME_PREFIX-`date +"%Y-%m-%d-%H-%M-%S"`
 echo Filename: $filename
-echo Paths:    $@
+tarsnap -c -f $filename $@
+filename=$FILENAME_PREFIX-latest
+echo Filename: $filename
 tarsnap -c -f $filename $@
