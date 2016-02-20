b653fe59f92dd45f817238efb6eb4a25f662606f
Andrew Heald <andrew@heald.co.uk>
2016-02-20 12:39:39 +0000

    Add verbose logging of backup and restore.

--- opt/backup.sh
+++ opt/backup.sh
@@ -3,6 +3,6 @@ echo Tarsnap Backup ...
 echo Paths: $@
 image=$IMAGE_PREFIX-`date +"%Y-%m-%d-%H-%M-%S"`
 echo Image: $image
-tarsnap -c -f $image $@
+tarsnap -c -f -v $image $@
 echo Cache size:
 du -sh /usr/local/tarsnap-cache
